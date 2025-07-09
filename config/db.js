2324d6e53c43e1cdecd82f7f5231ea9deeeaee37
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-07-09 23:57:05 +0500

    db change

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config(); // 👈 load environment variables
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: 'gillbaba.com',
+  user: 'u167227426_orient_walls',
+  password: 'Orientwalls@122',
+  database: 'u167227426_orient_walls',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
